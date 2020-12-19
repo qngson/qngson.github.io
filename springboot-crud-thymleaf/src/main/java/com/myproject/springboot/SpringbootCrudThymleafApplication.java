232dbdd6594fe528f007d9d49ec1bfa200c7766b
Admin <Admin@Admin.mshome.net>
2020-12-19 09:10:16 +0700

    spring boot crud

---
+++ springboot-crud-thymleaf/src/main/java/com/myproject/springboot/SpringbootCrudThymleafApplication.java
@@ -0,0 +1,15 @@
+package com.myproject.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@Component
+public class SpringbootCrudThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudThymleafApplication.class, args);
+	}
+
+}
